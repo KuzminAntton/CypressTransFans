5441f11a749d8cf2899bfe9fc87c3dd423aa5a6a
e-kurilenkoWhisk <106955020+e-kurilenkoWhisk@users.noreply.github.com>
2023-12-21 22:39:13 +0100

    fix methods and test Login

--- cypress/pages/login/loginPage.cy.js
+++ cypress/pages/login/loginPage.cy.js
@@ -4,7 +4,7 @@ class LoginPage {
     constructor() {
         this.ERROR_EMAIL_MESSAGE = 'email must be a valid email';
         this.WRONG_EMAIL_OR_PASSWORD_MESSAGE = 'Wrong email or password';
-        this.NOT_REGISTRATED_USER_MESSAGE = 'Oops ! Seems like you are not registered with us , please click on Sign up to with new user or try again with a different email address';
+        this.NOT_REGISTRATED_USER_MESSAGE = 'The email you have entered is not registered with us. Please sign up and try again.';
 
     }
 
@@ -15,7 +15,7 @@ class LoginPage {
         passwordField: () => cy.get('#input-field-password').should('be.visible'),
         submitButton: () => cy.get('[type="submit"]').should('be.visible'),
         signUpButton: () => cy.get('[href="/registration"]').should('be.visible'),
-        emailError: () =>  cy.xpath('p//class="MuiFormHelperText-root Mui-error MuiFormHelperText-sizeMedium MuiFormHelperText-filled css-zfjn7v"').should('be.visible'),
+        emailError: () =>  cy.get('[class="MuiFormHelperText-root Mui-error MuiFormHelperText-sizeMedium MuiFormHelperText-filled css-zfjn7v"]').should('be.visible'),
         wrongEmailOrPassword: () => cy.get('[class="MuiAlert-message css-q3hi8p"]').should('be.visible')
     };
 
@@ -25,7 +25,11 @@ class LoginPage {
         this.elements.passwordField().type(password);
         this.elements.submitButton().click();
     }
-
+    logInButNotClickSubmitButton(loginName, password) {
+        this.verifyThatIsLoginPage();
+        this.elements.loginField().type(loginName);
+        this.elements.passwordField().type(password);
+    }
     clickOnSignUpButton() {
         this.elements.signUpButton().click();
     }
@@ -35,11 +39,13 @@ class LoginPage {
     }
 
     verifyEmailErrorMessageIsVisible() {
+        setTimeout(this.elements.signUpButton,500)
         this.elements.emailError().should('be.visible')
         this.elements.emailError().should('contain', this.ERROR_EMAIL_MESSAGE)
     }
 
     verifyWrongEmailOrPasswordErrorMessageIsVisible() {
+        setTimeout(this.elements.signUpButton,500)
         this.elements.wrongEmailOrPassword().should('be.visible')
         this.elements.wrongEmailOrPassword().should('contain', this.WRONG_EMAIL_OR_PASSWORD_MESSAGE)
     }
